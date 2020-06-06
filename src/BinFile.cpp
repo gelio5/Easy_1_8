51a62fc6729bca4b063e5082b48ff1b6a56aab0b
gelio5 <vlreznik97@gmail.com>
2020-06-07 02:54:19 +0300

    update

--- src/BinFile.cpp
+++ src/BinFile.cpp
@@ -18,14 +18,18 @@ std::vector<uint_fast8_t> read(const std::string& fileName)
 {
     std::ifstream input;
     input.open(fileName, std::ios::binary);
+
     if (!input)
         throw std::invalid_argument("File with name \"" + fileName + "\" does not exist");
+
     struct stat fileInfo{};
     stat(fileName.c_str(), &fileInfo);
     int fileSize = fileInfo.st_size;
     std::vector<uint_fast8_t> readableData(fileSize);
+
     for (auto& position : readableData)
         input >> position;
+
     input.close();
     return readableData;
 }
